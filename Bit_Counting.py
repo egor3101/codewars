fa07ff4dbf3a7d1e5a1e5b2eedfe5af1f2ae916f
egor3101 <eszxdrtfc14@mail.ru>
2023-10-07 02:09:07 +0300

    Describe of task is added. +1 task (Fib_sequence)

--- Bit_Counting.py
+++ Bit_Counting.py
@@ -1,3 +1,7 @@
+"""Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case"""
+
 n = 1234
 binary_num = format(n, 'b')
 x = [int(i) for i in str(binary_num)]
--- Tree_Photography.py
+++ Tree_Photography.py
@@ -1,3 +1,25 @@
+"""Task
+Heading off to the Tree Arboretum of Various Heights, I bring along my camera to snap up a few photos. Ideally, I'd want to take a picture of as many trees as possible, but the taller trees may cover up the shorter trees behind it.
+
+A tree is hidden if it is shorter than or the same height as a ( any ) tree in front of it, as seen in a particular direction.
+
+Given a list of tree heights, create a function which returns "left" or "right", depending on which side allows me to see as many trees as possible.
+
+Worked Example
+[1, 3, 1, 6, 5] ➞ "left"
+// If I stand on the left, I can see trees of heights 1, 3 and 6.
+// If I stand on the right, I can see trees of heights 5 and 6.
+// Return "left" because I would see more trees.
+Examples
+[5, 6, 5, 4] ➞ "right"
+
+[1, 2, 3, 3, 3, 3, 3] ➞ "left"
+
+[3, 1, 4, 1, 5, 9, 2, 6] ➞ "left"
+Notes
+There will always be a best side."""
+
+
 lst = [3, 1, 4, 1, 5, 9, 2, 6]
 now_thee = 0
 now_thee_for_rev = 0
